02f50aa6bcf4b97664a6cdc28517aad3b154c081
yesi07111 <yeseniavr07111@gmail.com>
2024-03-31 09:16:24 -0400

    External API Configurations

---
+++ Application/Configurations/ExternalApiConfiguration.cs
@@ -0,0 +1,8 @@
+namespace Application.Configurations;
+
+public class ExternalApiConfiguration
+{
+    public string BaseUrl { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+
+}
