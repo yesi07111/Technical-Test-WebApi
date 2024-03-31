a41540fd0f36d9d8562388a5a7df1f23e0d9d56c
yesi07111 <yeseniavr07111@gmail.com>
2024-03-31 09:35:25 -0400

    Add Migrations

---
+++ Infraestructure/Migrations/Postgres/20240330162426_AddWeatherForecast.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infraestructure.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class AddWeatherForecast : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherConditions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    Icon = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherConditions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeatherForecasts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RegionName = table.Column<string>(type: "text", nullable: false),
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    TempC = table.Column<double>(type: "double precision", nullable: false),
+                    FeelsLikeC = table.Column<double>(type: "double precision", nullable: false),
+                    WindKph = table.Column<double>(type: "double precision", nullable: false),
+                    Humidity = table.Column<int>(type: "integer", nullable: false),
+                    ConditionId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherForecasts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WeatherForecasts_WeatherConditions_ConditionId",
+                        column: x => x.ConditionId,
+                        principalTable: "WeatherConditions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeatherForecasts_ConditionId",
+                table: "WeatherForecasts",
+                column: "ConditionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherForecasts");
+
+            migrationBuilder.DropTable(
+                name: "WeatherConditions");
+        }
+    }
+}
---
+++ Infraestructure/Migrations/Postgres/20240330195358_Modify2WeatherForecast.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infraestructure.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class Modify2WeatherForecast : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "WindKph",
+                table: "WeatherForecasts",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TempC",
+                table: "WeatherForecasts",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FeelsLikeC",
+                table: "WeatherForecasts",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "WindKph",
+                table: "WeatherForecasts",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "TempC",
+                table: "WeatherForecasts",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "FeelsLikeC",
+                table: "WeatherForecasts",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
